e6967acc11ed00fee133d4db317afebad5a4e9ae
gbemme <gbemikp@gmail.com>
2020-04-03 13:29:25 +0100

    swagger api

--- web/src/main/java/com/project/nairon/payment/controller/AdvertiserPaymentController.java
+++ web/src/main/java/com/project/nairon/payment/controller/AdvertiserPaymentController.java
@@ -3,19 +3,26 @@
  */
 package com.project.nairon.payment.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import
+		org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flutterwave.rave.java.payload.transferPayload;
 import com.project.nairon.flutter.payment.service.TransferService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * @author gbemisola
  *
  */
 @RestController
+@RequestMapping("/api")
+@Tag(name = "Payment", description = "the PAYMENT API")
 public class AdvertiserPaymentController {
 	
 	@Autowired
