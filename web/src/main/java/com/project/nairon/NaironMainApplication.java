203899144696511b21ca3295137346b5a7fabd1a
tbodv1 <tboydv1@gmail.com>
2020-04-03 09:25:47 +0100

    project structure update

---
+++ web/src/main/java/com/project/nairon/NaironMainApplication.java
@@ -0,0 +1,14 @@
+package com.project.nairon;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaironMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaironMainApplication.class);
+    }
+
+}
