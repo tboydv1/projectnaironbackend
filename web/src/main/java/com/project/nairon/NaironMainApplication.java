eba106d2d7046c7df5038585d7a8e860b732172b
tbodv1 <tboydv1@gmail.com>
2020-04-06 09:13:44 +0100

    Registration controller complete

--- web/src/main/java/com/project/nairon/NaironMainApplication.java
+++ web/src/main/java/com/project/nairon/NaironMainApplication.java
@@ -1,8 +1,10 @@
 package com.project.nairon;
 
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.project.nairon")
--- web/src/main/java/com/project/nairon/controller/naironuser/NaionUserController.java
+++ web/src/main/java/com/project/nairon/controller/naironuser/NaionUserController.java
@@ -7,32 +7,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.logging.Logger;
 
 @RestController
+@RequestMapping("/api")
 public class NaionUserController {
 
     Logger logger = Logger.getLogger(getClass().getName());
 
+
     @Autowired
     private NaironUserService naironUserService;
+
 
     @GetMapping("/")
     public String test() {
         return "Application is live and running";
     }
 
-    @PostMapping("/user/start")
-    public ResponseEntity<NaironUser> registerUserAccount(NaironUserDTO naironUserDTO, HttpRequest request){
+
+    @PostMapping("/start")
+    public ResponseEntity<NaironUser> registerUserAccount(@RequestBody NaironUserDTO naironUserDTO){
 
         logger.info("Registration user account with information: {}"+ naironUserDTO);
-        NaironUser registered = naironUserService.registerNewUserAccount(naironUserDTO);
 
+        NaironUser registered = naironUserService.registerNewUserAccount(naironUserDTO);
 
         return new ResponseEntity<>(registered, HttpStatus.OK);
     }
+
 }
